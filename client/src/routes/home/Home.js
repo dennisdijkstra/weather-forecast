90d7fc647b182de960881fc61fc391fe74dcaa3d
Dennis <info@dennisdijkstra.nl>
2019-01-31 23:00:56 +0100

    add form, use autocomplete and get lat lng based on address

--- client/src/routes/home/Home.js
+++ client/src/routes/home/Home.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import { Formik } from 'formik';
 import s from './home.css';
 
 class Home extends Component {
+    getLatLng = async ({ location }) => {
+        const [results] = await geocodeByAddress(location);
+        const { lat, lng } = await getLatLng(results);
+        console.log(`${lat} ${lng}`);
+    }
+
     render() {
         return (
-            <h1>Hello World</h1>
+            <Formik
+                initialValues={{ location: '' }}
+                onSubmit={this.getLatLng}
+            >
+                {({ values, handleSubmit, setFieldValue }) => (
+                    <form onSubmit={handleSubmit}>
+                        <PlacesAutocomplete
+                            value={values.location}
+                            onChange={value => setFieldValue('location', value)}
+                            onSelect={this.handleSelect}
+                        >
+                            {({ getInputProps, getSuggestionItemProps, suggestions }) => (
+                                <>
+                                    <input
+                                        {...getInputProps({
+                                            placeholder: 'Enter a city',
+                                        })}
+                                    />
+                                    <div>
+                                        {suggestions.map(suggestion => (
+                                            <div {...getSuggestionItemProps(suggestion)}>
+                                                <span>{suggestion.description}</span>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </>
+                            )}
+                        </PlacesAutocomplete>
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+            </Formik>
         );
     }
 }
