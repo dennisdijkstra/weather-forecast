e1c2065dbb5bb784815e7a26e21e197cacf5f865
Dennis <info@dennisdijkstra.nl>
2019-02-01 21:46:37 +0100

    implement datepicker, request weather on certain date

--- client/src/routes/home/Home.js
+++ client/src/routes/home/Home.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import { Formik } from 'formik';
 import s from './home.css';
 
 class Home extends Component {
-    getLatLng = async ({ location }, { resetForm }) => {
+    getLatLng = async ({ location, date }, { resetForm }) => {
+        const formattedDate = moment(date).format();
         const [results] = await geocodeByAddress(location);
-        const data = await getLatLng(results);
+        const { lat, lng } = await getLatLng(results);
+        const data = { lat, lng, formattedDate };
 
-        resetForm({ location: '' });
+        resetForm({ location: '', date: moment().toDate() });
         this.getWeather(data);
     }
 
@@ -26,44 +31,53 @@ class Home extends Component {
 
     render() {
         return (
-            <Formik
-                initialValues={{ location: '' }}
-                onSubmit={this.getLatLng}
-            >
-                {({
-                    values,
-                    dirty,
-                    handleSubmit,
-                    isSubmitting,
-                    setFieldValue
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <PlacesAutocomplete
-                            value={values.location}
-                            onChange={value => setFieldValue('location', value)}
-                            onSelect={this.handleSelect}
-                        >
-                            {({ getInputProps, getSuggestionItemProps, suggestions }) => (
-                                <>
-                                    <input
-                                        {...getInputProps({
-                                            placeholder: 'Enter a location',
-                                        })}
-                                    />
-                                    <div className={s.suggestions}>
-                                        {suggestions.map(suggestion => (
-                                            <div {...getSuggestionItemProps(suggestion)}>
-                                                <span className={s.suggestion}>{suggestion.description}</span>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </>
-                            )}
-                        </PlacesAutocomplete>
-                        <button type="submit" disabled={!dirty || isSubmitting} className={s.button}>Get Weather</button>
-                    </form>
-                )}
-            </Formik>
+            <div>
+                <h1>Get observed or forecasted weather conditions</h1>
+                <Formik
+                    initialValues={{ location: '', date: moment().toDate() }}
+                    onSubmit={this.getLatLng}
+                >
+                    {({
+                        values,
+                        dirty,
+                        handleSubmit,
+                        isSubmitting,
+                        setFieldValue
+                    }) => (
+                        <form onSubmit={handleSubmit}>
+                            <PlacesAutocomplete
+                                value={values.location}
+                                onChange={value => setFieldValue('location', value)}
+                                onSelect={this.handleSelect}
+                            >
+                                {({ getInputProps, getSuggestionItemProps, suggestions }) => (
+                                    <>
+                                        <input
+                                            {...getInputProps({
+                                                placeholder: 'Enter a location',
+                                            })}
+                                        />
+                                        <div className={s.suggestions}>
+                                            {suggestions.map(suggestion => (
+                                                <div {...getSuggestionItemProps(suggestion)}>
+                                                    <span className={s.suggestion}>{suggestion.description}</span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                        <DatePicker
+                                            selected={values.date}
+                                            onChange={value => setFieldValue('date', value)}
+                                            minDate={moment().subtract(30, 'days').toDate()}
+                                            maxDate={moment().add(30, 'days').toDate()}
+                                        />
+                                    </>
+                                )}
+                            </PlacesAutocomplete>
+                            <button type="submit" disabled={!dirty || isSubmitting} className={s.button}>Get Weather</button>
+                        </form>
+                    )}
+                </Formik>
+            </div>
         );
     }
 }
--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const moment = require('moment');
 const secretKey = require('../../config/keys').secretKey;
 
 const router = express.Router();
 
-router.post('/weather', async ({ body: { lat, lng } }, res) => {
+router.post('/weather', async ({ body: { lat, lng, formattedDate } }, res) => {
     try {
-        const time = moment().format();
-        const response = await fetch(`https://api.darksky.net/forecast/${secretKey}/${lat},${lng},${time}?units=si`);
+        const response = await fetch(`https://api.darksky.net/forecast/${secretKey}/${lat},${lng},${formattedDate}?units=si`);
         const data = await response.json();
 
         res.send(data);
